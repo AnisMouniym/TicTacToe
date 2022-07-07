4bd70ef7b462bd617d0d1fd33fa18c45dcb60853
AnisMouniym <95011291+AnisMouniym@users.noreply.github.com>
2022-07-07 11:57:21 +0200

    create file

---
+++ tictactoe/src/Square.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Sqaure extends React.Component {
+    render() {
+        return (
+            <button className="square">
+                
+            </button>
+            )
+    }
+}
